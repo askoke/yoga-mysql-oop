547093fb7372d5f4070de6d9457a0cb5fa4008b6
Asko <hulkko54@gmail.com>
2023-11-01 15:18:55 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// application packages
 const express = require('express')
 const app = express()
 
@@ -6,7 +7,7 @@ const path = require('path')
 const hbs = require('express-handlebars');
 //setup template engine directory and files extensions
 app.set('views', path.join(__dirname, 'views'))
-app.set('view-engine', 'hbs');
+app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
@@ -18,20 +19,33 @@ app.use(express.static('public'))
 const mysql = require('mysql')
 
 const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // create database connection
 var con = mysql.createConnection({
     host: "localhost",
     user: "asko",
     password: "password",
-    database: "yoga_mysql"
+    database: "joga_mysql"
 })
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected to joga_mysql db");
 })
+
+// show all articles - index page
+app.get('/', (req, res) => {
+    let query = 'SELECT * FROM article';
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        res.render('index', {
+            articles: articles
+        })
+    })
+});
 
 // app start point
 app.listen(3000, () => {
